67d1a6b598729e49e524ca2a7377c83a15714f4e
Surendra-gurjar <surendragurjar7731@gmail.com>
2024-11-28 22:05:21 +0530

    rbac system with finel

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["Admin","Moderator", "User" ], default: "User" },
+  role: { type: String, enum: ["Admin", "User" ], default: "User" },
 });
 
 UserSchema.pre("save", async function (next) {
